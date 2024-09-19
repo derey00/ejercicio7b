9c00cd728e8798b1f587b695f2a419138bb67771
derey00 <71334124+derey00@users.noreply.github.com>
2024-09-19 10:38:39 +0200

    Add files via upload

---
+++ src/test/java/elena/ejercicio7/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package elena.ejercicio7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
